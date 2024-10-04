6c85c82969879667ae1124b4ab2276ed2649779d
arushi18p <66833087+arushi18p@users.noreply.github.com>
2024-10-04 18:12:19 -0400

    added information about myself

--- Intro.java
+++ Intro.java
@@ -1,3 +1,4 @@
 //Welcome to week 1 of blueprint!
 //Hi my name is Jane and here is a fun fact about me!
 //Hi my name is John and I love blueprint! 
+//Hi my name is Arushi and my favorite song is Rush by Troye Sivan at the moment!
