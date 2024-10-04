0b18f32cfc9b0980ad0907070ce66155ac9d4fbf
shriyashrestha2 <shriyaruby@gmail.com>
2024-10-04 18:23:39 -0400

    Update Intro.java

--- Intro.java
+++ Intro.java
@@ -2,3 +2,4 @@
 //Hi my name is Jane and here is a fun fact about me!
 //Hi my name is John and I love blueprint! 
 //Hi my name is Arushi and my favorite song is Rush by Troye Sivan at the moment!
+// Hi my name is Shriya and I love to dance
